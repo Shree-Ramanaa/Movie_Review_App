dceb590b2bebe32b48253d705ef02df5122fb555
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2023-08-08 09:34:03 +0530

    Add files via upload

---
+++ Backend/api/reviewsController.js
@@ -0,0 +1,109 @@
+import ReviewsDAO from "../dao/reviewsDAO.js"
+
+export default class ReviewsCtrl{
+
+    static async apiPostReview(req, res, next){
+        try{
+            const movieId = parseInt(req.body.movieId);
+            const review = req.body.review;
+            const user = req.body.user;
+
+            const reviewResponse = await ReviewsDAO.addReview(
+                movieId,
+                user,
+                review
+            )
+
+            res.json({status: "success"});
+
+        }catch(e){
+            res.status(500).json({error : e.message});
+        }
+    }
+
+    static async apiGetReview(req, res, next){
+        try{
+            let id = req.params.id || {}
+            let reviews = await ReviewsDAO.getReview(id);
+
+            if(!reviews){
+                res.status(404).json({error : "Not found"});
+                return;
+            }
+
+            if(reviews.error){
+                throw new Error("Invalid Id");
+            }
+
+            res.json(reviews);
+
+        }catch(e){
+            res.status(500).json({error:e.message});
+        }
+    }
+
+    static async apiUpdateReview(req, res, next){
+        try{
+            const reviewId = req.params.id;
+            const review = req.body.review;
+            const user = req.body.user;
+
+            const reviewResponse = await ReviewsDAO.updateReview(
+                reviewId,
+                user,
+                review
+            )
+
+            var {error} = reviewResponse;
+            if(error){
+                res.status(400).json({error});
+            }
+            
+            if(reviewResponse.modifiedCount === 0){
+                throw new Error(
+                    "Unable to update review"
+                )
+            }
+
+            res.json({status : "success"});
+
+        }catch(e){
+            res.status(500).json({error : e.message});
+        }
+    }
+
+    static async apiDeleteReview(req, res, next) {
+        try {
+          const reviewId = req.params.id
+          const reviewResponse = await ReviewsDAO.deleteReview(reviewId)
+          if(!reviewResponse.acknowleged){
+            throw new Error("unable to delete");
+          };
+          res.json({status : "success"});
+
+        } catch (e) {
+          res.status(500).json({ error: e.message })
+        }
+      }
+
+static async apiGetReviews(req, res, next) {
+        try {
+          let id = parseInt(req.params.id) || {};
+          let reviews = await ReviewsDAO.getReviewsByMovieId(id);
+
+          if (!reviews) {
+            res.status(404).json({ error: "Not found" })
+            return
+          }
+
+          if(reviews.error){
+            throw new Error("Invalid Id");
+          }
+          
+          res.json(reviews);
+          
+        } catch (e) {
+          res.status(500).json({ error: e.message })
+        }
+    }
+}
